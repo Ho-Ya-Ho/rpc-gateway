bb60177457859664aeca0e9684c379bd35af396c
Ho-Ya-Ho <rkdgh1056@gmail.com>
2026-04-03 14:07:11 +0900

    chore: initialize project

---
+++ src/main/java/com/hoya/rpc_gateway/RpcGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hoya.rpc_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoya/rpc_gateway/RpcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoya.rpc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
